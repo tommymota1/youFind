01f552f88e3bd2a4fcc6d64b3c565598bbad4dae
Tommy Mota <thomas.mota@marquette.edu>
2018-09-27 00:07:04 -0500

    Initial Commit

---
+++ youFind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  youFind
+//
+//  Created by Tommy  Mota on 9/27/18.
+//  Copyright © 2018 Mota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
